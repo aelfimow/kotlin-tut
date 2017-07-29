2674573caf888d2cf0b457eec3c4a7338501385c
Alexander Elfimow <my.free.mail@gmx.de>
2017-07-29 20:48:08 +0200

    hello-world: added functions

--- src/hello-world/hello-world.kt
+++ src/hello-world/hello-world.kt
@@ -1,4 +1,12 @@
 fun main(args: Array<String>) {
+    print_german();
+    print_english();
+}
+
+fun print_german() {
     println("Hallo, Welt!")
+}
+
+fun print_english() {
     println("Hello, World!")
 }
