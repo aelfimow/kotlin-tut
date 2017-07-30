fed836bfa0413dd8fe1d84e5af496772b228059e
Alexander Elfimow <my.free.mail@gmx.de>
2017-07-30 22:41:53 +0200

    string-template: added files

---
+++ src/string-template/string-template.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var str1 = "string 1"
+    var str2 = "string 2"
+
+    var str3 = "str1 is $str1 and length is ${str1.length}"
+    var str4 = "str2 is $str2 and length is ${str2.length}"
+
+    println(str3)
+    println(str4)
+}
