ea105d14cb7638852d0710ebd3bca112cc634b25
Alexander Elfimow <my.free.mail@gmx.de>
2017-08-01 21:23:55 +0200

    cond-expr: added further funs

--- src/cond-expr/cond-expr.kt
+++ src/cond-expr/cond-expr.kt
@@ -1,8 +1,12 @@
 fun main(args: Array<String>) {
     println("maxOf(1, 2): ${maxOf(1, 2)}")
+    println("maxOf(1, 2, 3): ${maxOf(1, 2, 3)}")
     println("minOf(1, 2): ${minOf(1, 2)}")
+    println("minOf(1, 2, 3): ${minOf(1, 2, 3)}")
 }
 
 fun maxOf(a: Int, b: Int) = if (a > b) a else b
+fun maxOf(a: Int, b: Int, c: Int) = maxOf(a, maxOf(b, c))
 
 fun minOf(a: Int, b: Int) = if (a > b) b else a
+fun minOf(a: Int, b: Int, c: Int) = minOf(a, minOf(b, c))
