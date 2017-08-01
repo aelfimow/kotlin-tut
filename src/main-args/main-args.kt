b124d672c29e208a71be4f9d4e41d10279ee8116
Alexander Elfimow <my.free.mail@gmx.de>
2017-08-01 21:54:03 +0200

    main-args: funs to print out args

--- src/main-args/main-args.kt
+++ src/main-args/main-args.kt
@@ -1,2 +1,29 @@
 fun main(args: Array<String>) {
+    println("args size: ${args.size}")
+    printAll(args)
+    checkNumbers(args)
+}
+
+fun printAll(args: Array<String>) {
+    var count = args.size;
+    while (count > 0) {
+        var tmp_index = args.size - count
+        println("args[$tmp_index]: ${args[tmp_index]}")
+        --count
+    }
+}
+
+fun checkNumbers(args: Array<String>) {
+    var count = args.size;
+
+    while (count > 0) {
+        var tmp_index = args.size - count
+        var number = args[tmp_index].toIntOrNull()
+        if (number != null) {
+            println("args[$tmp_index]: ${args[tmp_index]} is a number $number")
+        } else {
+            println("args[$tmp_index]: ${args[tmp_index]} is not a number")
+        }
+        --count
+    }
 }
