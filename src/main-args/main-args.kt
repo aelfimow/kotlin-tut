ffd4824c0a0a1b4629908df99fc543b555b0f6b0
Alexander Elfimow <my.free.mail@gmx.de>
2017-08-02 22:15:22 +0200

    Added function

--- src/main-args/main-args.kt
+++ src/main-args/main-args.kt
@@ -1,15 +1,24 @@
 fun main(args: Array<String>) {
     println("args size: ${args.size}")
     printAll1(args)
+    printAll2(args)
     checkNumbers(args)
 }
 
 fun printAll1(args: Array<String>) {
+    println("Printin out args (method 1):")
     var count = args.size;
     while (count > 0) {
         var tmp_index = args.size - count
         println("args[$tmp_index]: ${args[tmp_index]}")
         --count
+    }
+}
+
+fun printAll2(args: Array<String>) {
+    println("Printin out args (method 2):")
+    for (one_arg in args) {
+        println("One arg: ${one_arg}")
     }
 }
 
