1296e9554df15f7f729369c619e439441b686859
Alexander Elfimow <my.free.mail@gmx.de>
2017-08-02 22:31:04 +0200

    for-loop: added for loops

--- src/for-loop/for-loop.kt
+++ src/for-loop/for-loop.kt
@@ -3,4 +3,13 @@ fun main(args: Array<String>) {
     for (fruit in myFruits) {
         println("fruit is $fruit")
     }
+    for (fruit_index in myFruits.indices) {
+        println("fruit[$fruit_index] is ${myFruits[fruit_index]}")
+    }
+    for (arg in args) {
+        println("arg is $arg")
+    }
+    for ((argc, argstr) in args.withIndex()) {
+        println("args[$argc] is $argstr")
+    }
 }
