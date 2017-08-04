1d0d184428f052f9f3e1636c86fb0f723f7a7781
Alexander Elfimow <my.free.mail@gmx.de>
2017-08-04 22:25:41 +0200

    ranges: added function

--- src/ranges/ranges.kt
+++ src/ranges/ranges.kt
@@ -1,2 +1,17 @@
 fun main(args: Array<String>) {
+    for (arg in args) {
+        Int value = arg.toIntOrNull()
+        if (value != null) {
+            checkRange(value)
+        }
+    }
+}
+
+fun checkRange(value: Int) {
+    if (value in 2 .. 16) {
+        println("$value is in range")
+    }
+    else {
+        println("$value is not in range")
+    }
 }
