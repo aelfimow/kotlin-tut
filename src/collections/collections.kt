6754d63823d8e8c600c65f886f0330149d768cb1
Alexander Elfimow <my.free.mail@gmx.de>
2017-08-09 22:04:52 +0200

    collections: added functions

--- src/collections/collections.kt
+++ src/collections/collections.kt
@@ -1,2 +1,23 @@
 fun main(args: Array<String>) {
+    when {
+        "Obst" in args -> collection_obst()
+        "Bauteile" in args -> collection_bauteile()
+    }
+}
+
+fun collection_obst() {
+    val obst = setOf("Apfel", "Birne", "Kiwi", "Banane")
+    when {
+        "Tomate" !in obst -> println("Tomate ist nicht da")
+        "Birne" in obst -> println("Birne ist da")
+    }
+}
+
+fun collection_bauteile() {
+    val bauteile = setOf("Widerstand", "Transistor", "Kondensator", "Spule")
+    when {
+        "Widerstand" in bauteile -> println("Widerstand ist da")
+        "Transistor" in bauteile -> println("Transistor ist da")
+        "IC" !in bauteile -> println("IC fehlt!")
+    }
 }
