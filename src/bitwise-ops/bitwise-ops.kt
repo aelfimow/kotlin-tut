27e64b69ecfe7fcc91f60cbc0a3aff172b0a44b5
Alexander Elfimow <my.free.mail@gmx.de>
2017-08-20 21:42:49 +0200

    bitwise-ops: added prints

--- src/bitwise-ops/bitwise-ops.kt
+++ src/bitwise-ops/bitwise-ops.kt
@@ -1,11 +1,27 @@
 fun main(args: Array<String>) {
     println("result: ${1 shl 1}")
+    println("result: ${1.shl(1)}")
+
     println("result: ${2 shr 1}")
+    println("result: ${2.shr(1)}")
+
     println("result: ${2 ushr 1}")
+    println("result: ${2.ushr(1)}")
+
     println("result: ${-1 shl 1}")
+    println("result: ${-1.shl(1)}")
+
     println("result: ${-2 shr 1}")
+    println("result: ${-2.shr(1)}")
+
     println("result: ${1 and 3}")
+    println("result: ${1.and(3)}")
+
     println("result: ${1 or 3}")
+    println("result: ${1.or(3)}")
+
     println("result: ${1 xor 3}")
+    println("result: ${1.xor(3)}")
+
     println("result: ${1.inv()}")
 }
